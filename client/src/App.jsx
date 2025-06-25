77d2c9b643f0f2ffd06856d658c2f53df1f76a0b
SonkarNitish <nitish8299sonkar@gmail.com>
2025-06-25 17:44:53 +0530

    Create Home Page

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {  Route, Routes, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Home from './pages/Home';
+
+const App = () => {
+
+  const isOwnerPath =useLocation().pathname.includes("owner");
+
+  return (
+    <div>
+      {!isOwnerPath && 
+      <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
