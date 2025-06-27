c3d5dc32c9f2e5bf137a6de93df207072cef9b94
SonkarNitish <nitish8299sonkar@gmail.com>
2025-06-27 20:14:16 +0530

    Created webpage displaying Hotel Rooms.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {  Route, Routes, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import AllRooms from './pages/AllRooms';
 
 const App = () => {
 
@@ -15,6 +16,8 @@ const App = () => {
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/rooms' element={<AllRooms />}/>
+
         </Routes>
       </div>
       <Footer />
