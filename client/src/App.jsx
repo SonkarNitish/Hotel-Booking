bcc0b1dc5f5a16a92db96c2fac0d57e7946602ad
SonkarNitish <nitish8299sonkar@gmail.com>
2025-06-28 16:41:12 +0530

    Created webpage disaplying Hotel Details.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
+import RoomDetails from './pages/RoomDetails';
 
 const App = () => {
 
@@ -17,6 +18,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/rooms' element={<AllRooms />}/>
+          <Route path='/rooms/:id' element={<RoomDetails />}/>
 
         </Routes>
       </div>
